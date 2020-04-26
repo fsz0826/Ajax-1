ac8b4541782f242e2bcc89b86533f604fa86530c
fsz <513302151@qq.com>
2020-04-26 18:39:15 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,111 @@
+// console.log("ajax");
+
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css");
+  request.onreadystatechange = () => {
+    //下载完成，但不知道是成功还是失败   成功2xx 失败4xx  5xx
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        //创建style标签
+        const style = document.createElement("style");
+        // 填写style内容
+        style.innerHTML = request.response;
+        //插入到head里面
+        document.head.appendChild(style);
+      } else {
+        alert("加载失败");
+      }
+    }
+  };
+  request.send();
+};
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const script = document.createElement("script");
+        script.innerHTML = request.response;
+        document.body.appendChild(script);
+      } else {
+        alert("请求失败");
+      }
+    }
+  };
+  request.send();
+};
+
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const div = document.createElement("div");
+        div.innerHTML = request.response;
+        document.body.appendChild(div);
+      } else {
+        alert("请求失败");
+      }
+    }
+  };
+  request.send();
+};
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const dom = request.responseXML;
+        const text = dom.getElementsByTagName("warning")[0].textContent;
+        console.log(text.trim());
+      } else {
+        alert("请求失败");
+      }
+    }
+  };
+  request.send();
+};
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const object = JSON.parse(request.response);
+        console.log(object);
+        myName.textContent = object.name;
+      } else {
+        alert("请求失败");
+      }
+    }
+  };
+  request.send();
+};
+
+let n = 1;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const array = JSON.parse(request.response);
+        console.log("1");
+        array.forEach((item) => {
+          const li = document.createElement("li");
+          li.textContent = item.id;
+          xxx.appendChild(li);
+        });
+        n += 1;
+      } else {
+        alert("请求失败");
+      }
+    }
+  };
+  request.send();
+};
